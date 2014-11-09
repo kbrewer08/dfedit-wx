013b3d148148cce1339943fd638405cf9a0dcdbc
Keith <keith.brewer.08@gmail.com>
2014-11-09 09:04:54 -0700

    Just a few cosmetic changes to the source

--- wxDFeditMain.cpp
+++ wxDFeditMain.cpp
@@ -9,43 +9,6 @@ using namespace std;
 enum wxbuildinfoformat {
     short_f, long_f };
 
-wxString wxbuildinfo(wxbuildinfoformat format)
-{
-    wxString wxbuild(wxVERSION_STRING);
-
-    if (format == long_f )
-    {
-#if defined(__WXMSW__)
-        wxbuild << _T("-Windows");
-#elif defined(__WXMAC__)
-        wxbuild << _T("-Mac");
-#elif defined(__UNIX__)
-        wxbuild << _T("-Linux");
-#endif
-        wxbuild << _T("-Unicode build");
-    }
-
-    return wxbuild;
-}
-
-wxString wxDFeditFrame::dfeditversion(wxString versionType)
-{
-    wxString version = wxString();
-    if(versionType == wxT("long"))
-    {
-        version << wxT("Dragon Force Edit ");
-        version << wxT(DFEDIT_VERSION);
-    } else if(versionType == "short"){
-        version << wxT("DFedit ");
-        version << wxT(DFEDIT_VERSION);
-    } else {
-        version << wxT("DFedit ");
-        version << wxT(DFEDIT_VERSION);
-    }
-
-    return version;
-}
-
 BEGIN_EVENT_TABLE(wxDFeditFrame, wxFrame)
     EVT_CLOSE(wxDFeditFrame::OnClose)
     EVT_MENU(idMenuQuit, wxDFeditFrame::OnQuit)
@@ -70,7 +33,6 @@ wxDFeditFrame::wxDFeditFrame(wxFrame *frame, const wxString& title)
 
     SetMenuBar(mbar);
 
-    // create a status bar with some information about the used wxWidgets version
     CreateStatusBar(1);
 
     wxString welcome = _("Welcome to ");
@@ -107,4 +69,41 @@ void wxDFeditFrame::OnAbout(wxCommandEvent &event)
     aboutTitle << dfeditversion(wxT("short"));
 
     wxMessageBox(msg, aboutTitle);
+}
+
+wxString wxbuildinfo(wxbuildinfoformat format)
+{
+    wxString wxbuild(wxVERSION_STRING);
+
+    if (format == long_f )
+    {
+#if defined(__WXMSW__)
+        wxbuild << _T("-Windows");
+#elif defined(__WXMAC__)
+        wxbuild << _T("-Mac");
+#elif defined(__UNIX__)
+        wxbuild << _T("-Linux");
+#endif
+        wxbuild << _T("-Unicode build");
+    }
+
+    return wxbuild;
+}
+
+wxString wxDFeditFrame::dfeditversion(wxString versionType)
+{
+    wxString version = wxString();
+    if(versionType == wxT("long"))
+    {
+        version << wxT("Dragon Force Edit ");
+        version << wxT(DFEDIT_VERSION);
+    } else if(versionType == "short"){
+        version << wxT("DFedit ");
+        version << wxT(DFEDIT_VERSION);
+    } else {
+        version << wxT("DFedit ");
+        version << wxT(DFEDIT_VERSION);
+    }
+
+    return version;
 }
