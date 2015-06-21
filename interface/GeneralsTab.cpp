e36bc23d67bc3dc747d423b3d989068c185523dd
Keith <keith.brewer.08@gmail.com>
2015-06-21 13:12:09 -0700

    sizer stuff

--- interface/GeneralsTab.cpp
+++ interface/GeneralsTab.cpp
@@ -9,29 +9,18 @@ GeneralsTab::GeneralsTab(wxWindow* parent, int id)
     : wxPanel(parent, id)
 {
     masterSizer = new wxGridSizer(2, 3, 50, 50);
-    
-    basicsStaticBox       = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Basic"));
-/*    weaknessesStaticBox   = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Weakness Against"));
-    statusInfoStaticBox   = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Status"));
-    medalsStaticBox       = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Troops & Medals"));
-    locationStaticBox     = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Location Editing"));
-    unknownStatsStaticBox = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Unknown"));
-*/
-    masterSizer->Add(basicsStaticBox,       0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
-/*    masterSizer->Add(weaknessesStaticBox,   0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
-    masterSizer->Add(statusInfoStaticBox,   0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
-    masterSizer->Add(medalsStaticBox,       0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
-    masterSizer->Add(locationStaticBox,     0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
-    masterSizer->Add(unknownStatsStaticBox, 0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
-*/
+
+    basicsStaticBox = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Basic"));
+
+    masterSizer->Add(basicsStaticBox, 0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
     basicsGroupBox = new wxGridBagSizer();
     basicsStaticBox->Add(basicsGroupBox);
 
-    DFTab* dftab = (DFTab*)GetParent();
+    //DFTab* dftab = (DFTab*)GetParent();
     //wxAuiNotebook* mt = (wxAuiNotebook*)(dftab->GetParent());
 
     generalsListLabel = new wxStaticText(basicsStaticBox->GetStaticBox(), idGeneralsListLabel, _("Generals"));
-
+    
     generalsListComboBox = new wxComboBox(basicsStaticBox->GetStaticBox(), idGeneralsListComboBox);
     generalsListComboBox->Append(_T("Wein"));
     generalsListComboBox->Append(_T("Goldark"));
