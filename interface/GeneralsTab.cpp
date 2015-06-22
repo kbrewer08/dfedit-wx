6cb596ed26f4fb99763edf6ac7c37d26b8942eb7
Keith <keith.brewer.08@gmail.com>
2015-06-21 23:57:14 -0700

    Working on Generals tab for real now

--- interface/GeneralsTab.cpp
+++ interface/GeneralsTab.cpp
@@ -18,17 +18,18 @@ GeneralsTab::GeneralsTab(wxWindow* parent, int id)
     locationStaticBox     = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Location"));
     unknownStatsStaticBox = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Unknown"));
 
-    masterSizer->Add(basicsStaticBox,       0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
-    masterSizer->Add(weaknessesStaticBox,   0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
-    masterSizer->Add(statusInfoStaticBox,   0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
-    masterSizer->Add(medalsStaticBox,       0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
-    masterSizer->Add(locationStaticBox,     0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
-    masterSizer->Add(unknownStatsStaticBox, 0, wxALIGN_CENTER_VERTICAL | wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
+    masterSizer->Add(basicsStaticBox,       0, wxALIGN_LEFT | wxEXPAND);
+    masterSizer->Add(weaknessesStaticBox,   0, wxALIGN_LEFT | wxEXPAND);
+    masterSizer->Add(statusInfoStaticBox,   0, wxALIGN_LEFT | wxEXPAND);
+    masterSizer->Add(medalsStaticBox,       0, wxALIGN_LEFT | wxEXPAND);
+    masterSizer->Add(locationStaticBox,     0, wxALIGN_LEFT | wxEXPAND);
+    masterSizer->Add(unknownStatsStaticBox, 0, wxALIGN_LEFT | wxEXPAND);
 
     generalsListLabel = new wxStaticText(basicsStaticBox->GetStaticBox(), idGeneralsListLabel, _("Generals"));
     basicsStaticBox->Add(generalsListLabel);
     
-    generalsListComboBox = new wxComboBox(basicsStaticBox->GetStaticBox(), idGeneralsListComboBox);
+    generalsListComboBox = new wxComboBox(basicsStaticBox->GetStaticBox(), idGeneralsListComboBox,
+                                          wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, wxCB_READONLY | wxTE_PROCESS_ENTER);
     generalsListComboBox->Append(_T("Wein"));
     generalsListComboBox->Append(_T("Goldark"));
     generalsListComboBox->Append(_T("Teiris"));
